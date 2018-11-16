7b292cd353342288e4c97bd0e25f8a83e6bd784d
Andrew <andrew.shewmaker@pitkincounty.com>
2018-11-16 09:00:59 -0700

    Updated Release
    Added -h HELP option, added comments to source code.

--- zippairs.py
+++ zippairs.py
@@ -15,37 +15,73 @@ def main(directory):
     # crawling through directory and subdirectories 
     for root, directories, files in os.walk(directory): 
         for filename in files: 
+        
+            # We only care if the file is .tif or .tfw
             if filename.endswith('.tif') or filename.endswith('.tfw'):
-                # join the two strings in order to form the full filepath. 
+            
+                # build the zip path (common for files with the same basename) 
                 zipname = os.path.join(root, filename.replace('.tif','').replace('.tfw','') + '.zip')
+                
+                #build the filepath for this file
                 filepath = os.path.join(root, filename) 
                 
+                # if we do not have an archive by this name, then create one
                 if not zipname in zips:
                     zips[zipname] = ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED, True)
                 
+                # add this file to the archive
                 zips[zipname].write(filepath, basename(filepath))
                 
+                # print a log of the archive path and file path
                 print('[' + zipname + '] src: ' + filepath )
-                # print('zip: ' + os.path.join(root, zipname))
+                
+            # print a log that we are ignoring this file
+            else:
+                print('ignoring: ' + filename)
     
+    # loop through each zip, validate contents, and close the archive file
     for z in zips:
-        print('closing: ' + z)
+        count = len(zips[z].infolist())
         zips[z].close()
-        zips[z].printdir()
+        
+        # we should only have 2 files in a valid archive
+        if (count == 2):
+            print('closing: ' + z)
+            zips[z].printdir()
+        # if archive is invalid, delete the zip file
+        else:
+            print('invalid zip: ' + str(count) + ' files, ' + z)
+            os.remove(z)
+        
     print('All files zipped successfully!')         
   
-  
+# Main Entry Point of Code
 if __name__ == "__main__": 
-    # print "This is the name of the script: ", sys.argv[0]
-    # print "Number of arguments: ", len(sys.argv)
-    # print "The arguments are: " , str(sys.argv)
-    # print "The directory provided is: " , sys.argv[1]
+    # default value for directory
+    dir = 'NO DIRECTORY PROVIDED'
     
-    dir = 'C:\\OSGeo4W64\\DirZip1'
-    
-    if len(sys.argv) > 1 : dir = sys.argv[1]
-    print("Source Directory: " + dir)
-    print
-    
-    #call the main method
-    main(dir) 
+    # check that we received an argument
+    if len(sys.argv) > 1:
+        if (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
+            print('ZipPairs.py HELP:')
+            print('  Zip sets of files in the source directory')
+            print('')
+            print('  usage: python zippairs.py SOURCE')
+            print('    SOURCE\t*Required*\tFull directory path')
+            print('    -h, --help\t Optional\tshow this help message and exit')
+            print
+            
+        else:
+            # set variable with directory argument
+            dir = sys.argv[1]
+            
+            # we could validate that the directory exists?
+            
+            # log the directory provided
+            print("Source Directory: " + dir)
+            print('')
+            
+            #call the main method
+            main(dir) 
+    else:
+        print(dir)
